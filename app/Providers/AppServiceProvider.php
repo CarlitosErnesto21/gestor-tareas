dcc5a63b9b32e7e6aefd2efddb0e7dd3c6c12234
CarlitosErnesto21 <ernesto.rosales354@gmail.com>
2025-11-13 18:02:16 -0600

    modificaciones finales

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        // Forzar HTTPS en producción
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
+        }
 
         // Configurar longitud predeterminada de cadenas para MySQL
         Schema::defaultStringLength(191);
